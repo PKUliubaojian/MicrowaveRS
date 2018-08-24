49a4aa634feb8968c56c39f697b2be7f37390192
Baojian <1327157006@qq.com>
2018-08-24 13:38:42 +0800

    Dry snow permittivity
    Permittivity of dry snow calculation codes.
    Some domcumenting codes for Sphereinclusion

---
+++ Matlab/Chapter4/DielectricDrysnow.m
@@ -0,0 +1,26 @@
+function eps=DielectricDrysnow(T,frequency,rho)
+    % The relative permittivity of dry snow:
+    % page 140~142 in Ulaby's book, Chapter 4-6
+    % By Baojian Liu,2018.8.24, Powered by Matlab 2018a
+    %Chapter 4-3 in Microwave Radar and Radiometric Remote sensing, Ulaby and Long, 2014. 
+    if(any(frequency>1e12|frequency<1e7))
+        msgID = 'MATLAB:inputError';
+        msgtext = 'frequency should be 10MHz-1000GHz.';
+        ME = MException(msgID,msgtext);
+        throw(ME)
+    end
+    if(any(T>0))
+        msgID = 'MATLAB:inputError';
+        msgtext = "Temperature should be <0 centigrade";
+        ME = MException(msgID,msgtext);
+        throw(ME)
+    end
+    %0.9167 g/cm^3 is the density of pure ice
+    v_i=0.9167./rho;
+    eps_ice=DielectricPureice(T,frequency);
+    % A emperical expression was used in volume fraction>=0.45 and <0.45,respectively.
+    eps_i=(1.4759.*v_i).^3;
+    eps_i(v_i<0.45)=1+1.4667.*v_i+1.435.*v_i.^3;
+    eps_ii=0.34.*v_i.*(-imag(eps_ice))./((1-0.42.*v_i).^2);
+    eps=eps_i-1i.*eps_ii;
+end
--- Matlab/Chapter4/SphereInclusion.m
+++ Matlab/Chapter4/SphereInclusion.m
@@ -1,3 +1,11 @@
 function eps=SphereInclusion(eps_host,eps_included,volume)
+    % The relative permittivity of the mixture of two hetergeneous materials:
+    % page 131 equation 4.36b
+    %   eps_host:       relative permittivity of the host substance
+    %   eps_included:   relative permittivity of the inclusions
+    %   volume:         volume fraction
+    %
+    % By Baojian Liu,2018.8.23, Powered by Matlab 2018a
+    %Chapter 4-3 in Microwave Radar and Radiometric Remote sensing, Ulaby and Long, 2014. 
     eps=eps_host+3.*volume.*eps_host.*(eps_included-eps_host)./(eps_included+2.*eps_host);
 end
